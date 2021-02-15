1e1ad55a04f98c48b468a5d2044fc5dc80daab13
Afanasegor <afanasegor3@mail.ru>
2021-02-15 15:25:01 +0300

    First commit

---
+++ BotTemplate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ConsoleBot
+{
+    class Program
+    {
+    	string token = " // -- // -- // -- // "
+
+        private static readonly TelegramBotClient bot = new TelegramBotClient(token);
+        
+        static void Main(string[] args)
+        {
+            bot.OnMessage += Bot_OnMessage;
+            bot.SetWebhookAsync("");
+
+            var me = bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+
+            bot.StartReceiving();
+            Console.ReadLine();
+            bot.StopReceiving();
+        }
+
+        private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Message msg = e.Message;
+
+            if (msg == null)
+            {
+                return;
+            }
+
+            if (msg.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {               
+                await bot.SendTextMessageAsync(msg.Chat.Id, msg.From.FirstName + " ");
+            }
+        }
+    }
+}
